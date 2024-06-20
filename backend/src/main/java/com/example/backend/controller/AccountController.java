0cdf9326f43795a9054827b09ee3c11298af5187
pchee <pchee1489@naver.com>
2024-06-20 12:56:17 +0900

    JWT로 로그인 보완

--- backend/src/main/java/com/example/backend/controller/AccountController.java
+++ backend/src/main/java/com/example/backend/controller/AccountController.java
@@ -7,9 +7,15 @@ import org.springframework.web.server.ResponseStatusException;
 
 import com.example.backend.entity.Member;
 import com.example.backend.repository.MemberRepository;
+import com.example.backend.service.JwtService;
+import com.example.backend.service.JwtServiceImpl;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -17,14 +23,26 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class AccountController {
+
   @Autowired
   MemberRepository memberRepository;
   
   @PostMapping("/api/account/login")
-  public int login(@RequestBody Map<String, String> params) {
+  public ResponseEntity<Void> login(@RequestBody Map<String, String> params, HttpServletResponse res) {
     Member member = memberRepository.findByEmailAndPassword(params.get("email"), params.get("password"));
+
     if (member != null) {
-      return member.getId(); 
+      JwtService jwtService = new JwtServiceImpl();
+      int id = member.getId();
+      String token = jwtService.getToken("id", id);
+
+      Cookie cookie = new Cookie("token", token);
+      cookie.setHttpOnly(true);
+      cookie.setPath("/");
+
+      res.addCookie(cookie);
+      
+      return ResponseEntity.ok().build();
     }
     throw new ResponseStatusException(HttpStatus.NOT_FOUND);
   }
